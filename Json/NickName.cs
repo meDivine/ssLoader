c6e37bca0da75ea525c86bbe076db39d51af46d5
BUTCHERS228 <snowstormz@yandex.ru>
2021-06-24 10:43:10 +0500

    Добавьте файлы проекта.

---
+++ Json/NickName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssLoader.Json
+{
+    public class NickName
+    {
+        public string Nick { get; set; }
+    }
+}
