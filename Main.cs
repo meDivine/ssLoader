6b332e7c4605af720d7e65b668aa1701eb2cc612
Divine <snowstormz@yandex.ru>
2021-06-28 22:40:35 +0500

    тестовая заготовка

--- Main.cs
+++ Main.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ssLoader.Arizona;
+using ssLoader.SampStoreAPI;
 
 namespace ssLoader
 {
@@ -17,20 +19,32 @@ namespace ssLoader
             InitializeComponent();
         }
 
+        static FolderBrowserDialog folderPath = new FolderBrowserDialog();
+        
         private void folderBrowse_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog folderPath = new FolderBrowserDialog();
+            var accountSender = new AddAccount();
             DialogResult res = folderPath.ShowDialog();
+
             if (res == DialogResult.OK)
             {
-                pathLabel.Text = $"Path: {folderPath.SelectedPath}";
+                pathLabel.Text = $"Path: {accountSender.ToBase64("Cfif1998")}";
             }
-            var Arizona = new Arizona.GetAccounts();
+            /*var Arizona = new Arizona.GetAccounts();
             
             foreach (var test in Arizona.CheckNameArizona(folderPath.SelectedPath))
             {
                 MessageBox.Show(test);
-            }
+            }*/
+           
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+             var accountSender = new AddAccount();
+            //  arzSend.ArizonaSender(folderPath.SelectedPath));
+            string password = "Cfif1998";
+            Task.Run(() => accountSender.SendApi("1c96b8c2e30f007345c42825d556a0b1", "95.181.158.75:7777", "20", null, "Lucian_Butchers", password, null, "testcode", "test title"));
         }
     }
 }
