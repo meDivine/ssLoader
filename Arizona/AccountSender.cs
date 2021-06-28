d8327f34211f093a8401e96b29a117b9c8b02351
Divine <snowstormz@yandex.ru>
2021-06-28 22:39:33 +0500

    оно теперь отправляет и получает - ура

--- Arizona/AccountSender.cs
+++ Arizona/AccountSender.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Newtonsoft.Json;
+using ssLoader.Json;
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using ssLoader.SampStoreAPI;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace ssLoader.Arizona
 {
-    class AccountSender
+    public class AccountSender
     {
+        public async Task ArizonaSender(string path)
+        {
+            try
+            {
+                var arizonaFormat = new ArizonaFormat();
+                var getAccounts = new GetAccounts();
+                var brainburgGoods = getAccounts.CheckNameArizona(path);
+                var accountSender = new AddAccount();
+                 foreach (var test in brainburgGoods)
+                 {
+                     var text = File.ReadAllText($@"{path}\Arizona RP\Brainburg\goods\{test}.json");
+                     var result = JsonSerializer.Deserialize<ArizonaFormat>(text.Replace("\r\n", ""));
+                     MessageBox.Show(result.nick);
+                     
+
+                 }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
     }
 }
