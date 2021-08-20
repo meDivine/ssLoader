4b15d04ac01a6096f65a5a05ac122843408d1009
BUTCHERS228 <snowstormz@yandex.ru>
2021-08-20 12:34:43 +0500

    asdf

--- SendData/AccountSender.cs
+++ SendData/AccountSender.cs
@@ -1,4 +1,5 @@
 ﻿using ssLoader.Json;
+using ssLoader.Json.CarPrice;
 using ssLoader.SampStoreAPI;
 using System;
 using System.IO;
@@ -11,15 +12,61 @@ namespace ssLoader.Arizona
     public class AccountSender
     {
         private string getCurrDir = Directory.GetCurrentDirectory();
-        private readonly Money money = new Money();
         int successArizona = 0;
 
-        private int countPrice(float money, int lvl, int startprice, int typeMoney, int typeLevel)
+        private int CountPrice(float money, int lvl, int startprice, int typeMoney, int typeLevel)
         {
             var moneyCount = (money / 1000000.0) * typeMoney;
             var levelCount = lvl * typeLevel;
             int result = startprice + levelCount + (int)moneyCount;
             return result;
+        }
+
+        private int CarPrice(string input, string type, int typeMoney, double coefficient)
+        {
+            string line;
+
+            double carp = 0;
+            var file = new StreamReader($@"{getCurrDir}\Config\Cars\{type}.txt");
+            double result = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] words = line.Split(new char[] { ':' });
+                string carname = words[0];
+                string carprice = words[1];
+                if (input.Contains(carname))
+                {
+                    carp += float.Parse(carprice);
+                    result = ((carp / 1000000.0) * coefficient) * typeMoney;
+                }
+                
+            }
+            return (int)result;
+        }
+
+        public string minifyLong(long value)
+        {
+            if (value >= 100000000000)
+                return (value / 1000000000).ToString("#,0") + "ккк";
+            if (value >= 10000000000)
+                return (value / 1000000000D).ToString("0.#") + "ккк";
+            if (value >= 100000000)
+                return (value / 1000000).ToString("#.0") + "кк";
+            if (value >= 10000000)
+                return (value / 1000000D).ToString("0.#") + "кк";
+            if (value >= 1000000)
+                return (value / 1000D).ToString("#.0") + "к";
+            if (value >= 100000)
+                return (value / 1000).ToString("#.0") + "к";
+            if (value >= 100000)
+                return (value / 1000D).ToString("#.0") + "к";
+            if (value >= 10000)
+                return (value / 1000D).ToString("0.#") + "к";
+            if (value >= 0)
+                return (value / 1).ToString("#.0") + "вирт";
+            if (value >= 1000)
+                return (value / 100D).ToString("#.0") + "к";
+            return value.ToString("#.0");
         }
 
         public async Task SendToSS(string path)
@@ -42,31 +89,32 @@ namespace ssLoader.Arizona
                 var moneyPrice = await JsonSerializer.DeserializeAsync<Money>(openStream);
 
                 using FileStream levelPriceJson = File.OpenRead(@$"{getCurrDir}\Config\Levels.json");
-                var levelPrice = await JsonSerializer.DeserializeAsync<Level>(openStream);
+                var levelPrice = await JsonSerializer.DeserializeAsync<Level>(levelPriceJson);
 
                 using FileStream startPriceJson = File.OpenRead(@$"{getCurrDir}\Config\StartPrice.json");
-                var startPrice = await JsonSerializer.DeserializeAsync<StartPrice>(openStream);
+                var startPrice = await JsonSerializer.DeserializeAsync<StartPrice>(startPriceJson);
                 #endregion
-
+                string line;
                 #region ArizonaRP
-               // var arizonaFormat = new ArizonaFormat();
+                // var arizonaFormat = new ArizonaFormat();
                 var brainburgGoods = getAccounts.CheckNameArizonaBrainburg(path);
-                var main = new Main();
                 foreach (var ARZ in brainburgGoods)
                 {
                     if (File.Exists($@"{path}\Arizona RP\Brainburg\goods\{ARZ}.json"))
                     {
+                        float carp = 0;
                         var text = File.ReadAllText($@"{path}\Arizona RP\Brainburg\goods\{ARZ}.json");
                         var result = JsonSerializer.Deserialize<ArizonaFormat>(text.Replace("\r\n", ""));
                         /* var text1 = File.ReadAllText($@"C:\Users\Fesenko.AP\source\repos\BUTCHERS228\ssLoader\bin\Debug\net5.0-windows\Config\Config.json");
                          var result1 = JsonSerializer.Deserialize<Config>(text1);*/
-                        var price = countPrice(result.money, result.lvl, startPrice.ArizonaRP, moneyPrice.ArizonaRP, levelPrice.ArizonaRP);
-                        MessageBox.Show($"{result.nick} ник | {result.money} | цена акк {price}");
+                        int price = CountPrice(result.money, result.lvl, startPrice.ArizonaRP, moneyPrice.ArizonaRP, levelPrice.ArizonaRP);
+                        int carpr = CarPrice(result.cars, "ARZ", moneyPrice.ArizonaRP, 0.6);
+                        
+                            MessageBox.Show($"{result.nick} ник | {result.money} | цена акк {price + carpr} | лвл {result.lvl} | Кары {carpr} | {result.cars}  | вирт {minifyLong(result.money)}");
                         successArizona++;
                     }
                     // await Task.Run(() => accountSender.SendApi(jsonConfig.api_key, result.ip, 10 + (int)priceMoney, null, result.nick, result.password, "", "хорошего дня :)", $"{result.lvl} уровень | {result.money}$ на руках"));
                 }
-                //main.setLabelTextFromThread(main.label1, "asd");
                 /*
                 var ChandlerGoods = getAccounts.CheckNameArizonaChandler(path);
                 foreach (var ARZ in ChandlerGoods)
